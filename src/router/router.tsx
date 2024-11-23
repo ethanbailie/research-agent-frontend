fca0e74c800aea360649f53c6804f33ae4a2b3e8
Chris Ragobeer <chris.ragobeer@gmail.com>
2024-11-23 11:40:23 -0500

    tailwind

---
+++ src/router/router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import { ScrollTop } from "../ScrollTop";
+
+const GlobalRouter = () => {
+  return (
+    <ScrollTop>
+      <Routes></Routes>
+    </ScrollTop>
+  );
+};
+
+export default GlobalRouter;
